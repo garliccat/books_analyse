511ad8a1867c9661accd19041c58367d715d5b1d
Anton Petkov <petkov.anton.o@gmail.com>
2019-10-24 16:34:54 +0300

    working on

--- chitai_gorod_analyser.py
+++ chitai_gorod_analyser.py
@@ -1,25 +1,31 @@
+'''
+Датасет был любезно предоставлен (нет) сайтом магазина читай-город.
+Этот анализ затевался изначально просто чтобы посчитать среднюю высоту книги для строительства (очередного) шкафчика.
+Но раз парсить, то парсить всё что можно. Поэтому помимо габаритов книги были собраны еще кое-какие данные.
+Парсера тут нет, датасет уже собран.
+'''
+
 import pandas as pd
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
 import seaborn as sns
+sns.set()
 
 df = pd.read_csv('chitai_gorod_dataset.csv', sep=';', encoding='utf-16')
 
-# print(df.head())
-# print(df.describe())
-print(df.columns)
+print('Количество записей: ', df.shape[0])
 
-# print(df[df['height'] == df.height.max()].index.values)
-# print(df[df['weight'] == df.weight.max()].index.values)
+''' Обрезаем аномалии по высоте книги и весу, которые попадают за 99ый и перед 1ый квантилем.
+Попадаются всякие адские фолианты или гигантские календари (которые почему-то у читай-города в разделе "книги")
+'''
+df = df[(df['height'] < df['height'].quantile(.999)) & (df['height'] > df['height'].quantile(.001))]
+df = df[(df['weight'] < df['weight'].quantile(.999)) & (df['weight'] > df['weight'].quantile(.001))]
 
-df.drop([df[df['height'] == df.height.max()].index.values[0], df[df['weight'] == df.weight.max()].index.values[0]], inplace=True)
+sns.pairplot(df[['height', 'length', 'weight']], kind='reg')
+plt.show()
 
-# sns.set(style="ticks", color_codes=True)
-sns.pairplot(df[['height', 'length', 'weight']], palette="husl")
-
-height = df[(df['height'] < df['height'].quantile(.9)) & (df['height'] > df['height'].quantile(.1))]['height'].dropna()
-
+height = df['height'].dropna()
 hist, bins = np.histogram(height, bins=2)
 # print('Hist = ', hist)
 # print('Proportion = ', hist[0] / hist[1])
@@ -30,12 +36,13 @@ print('Height of additional (big) shelves (plus 3 cm.) = ', round((height.max() 
 print(height[height > bins[1]].count())
 print(height[height < bins[1]].count())
 
-prices = df[(df['price'] < df['price'].quantile(.9)) & (df['price'] > df['price'].quantile(.1))]['price'].dropna()
+sns.distplot(height, bins=2)
+plt.show()
+
+# prices = df[(df['price'] < df['price'].quantile(.9)) & (df['price'] > df['price'].quantile(.1))]['price'].dropna()
+prices = df['price'].dropna()
 hist, bins = np.histogram(prices)
 print('')
-print(hist)
-print(np.where(hist == hist.max()))
-print(bins)
 print('Most common price = ', np.average([bins[np.where(hist == hist.max())[0]], bins[np.where(hist == hist.max())[0] + 1]]))
 print('Median price = ', np.median(prices.array))
 print('Average price = ', np.mean(prices.array))
@@ -46,16 +53,21 @@ print('\nTop books by release year:')
 print(df.groupby(by='year').count()['title'].sort_values(ascending=False).head())
 print('\nTop books by page number:')
 pages = df.groupby(by='pages').count()['title'].sort_values(ascending=False)
-print('\n', pages.head())
+print(pages.head())
 page_conspiracy = df[df['pages'] == pages.index[0]].groupby(by='publisher').count()['title'].sort_values(ascending=False)
 print('\n', page_conspiracy.head())
 print('\nMost lazy publisher is: ', page_conspiracy.index[0])
 
+############ ONLY FOR CHITAI GOROD ############
 print('\nBooks ratings:')
-print(df.groupby(by='book_pg').count()['title'].sort_values(ascending=False).head()) ############ ONLY FOR CHITAI GOROD
+print(df.groupby(by='book_pg').count()['title'].sort_values(ascending=False).head()) 
+
+print('\nСамая дорогая книга:')
+print(df[df['price'] == df['price'].max()][['author', 'title', 'price']])
+sns.regplot(x='weight', y='price', data=df)
+plt.show()
 
 # sns.distplot(prices.array, bins=10)
-# sns.distplot(height, bins=2)
-# sns.jointplot(x='height', y='length', data=df)
+# sns.boxplot(prices.array, hue=0.7, linewidth=2.5)
 
-plt.show()
+# sns.jointplot(x='height', y='length', data=df)
